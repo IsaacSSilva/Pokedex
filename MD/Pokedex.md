e6941d4fcc9cf59375b3783a54d245aa0903caa9
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-07-25 12:59:15 -0300

    🎉
    Add logic do código juntamente com a integração da API: PokeAPI

--- MD/Pokedex.md
+++ MD/Pokedex.md
@@ -11,9 +11,7 @@
 **📄 Descrição | Proposta** </br>
 Um projeto no qual sera utilizado uma <a href="https://pokeapi.co/?ref=public-apis" >API</a> para buscar informacao direto da mesma e nos retornado na tela, como imagens, nome, tipo do pokemon, foi desidico ser realizado esse projeto para treinar e aprender como inplementrar API aos projetos e demais.
 
-</br>
-
-<a href="https://efficient-sloth-d85.notion.site/Desafio-Sidebar-f2251eb4976941eb958326ea327ffeb9">Desafio 👆</a>
+<a href="https://efficient-sloth-d85.notion.site/Desafio-Sidebar-f2251eb4976941eb958326ea327ffeb9">Projeto 👆</a>
 
 ---
 <!-- tecnologia utilizada -->
@@ -28,7 +26,7 @@ Um projeto no qual sera utilizado uma <a href="https://pokeapi.co/?ref=public-ap
 > ### <a href="https://github.com/IsaacGSS"> ***@Isaac S. Silva***</a>
 ---
 <!-- resumo e/ou descrição da comclusão -->
-<q> </q>
+<q>Assisti ao um video mostrando como se implementava a PokeAPI, logo em seguida construi esse projetos com poucas diferencias divuais e no codigo, porem fui eu mesmo que costrui ela, logo mais penso em doficiaar ela, ento no layout, o codo em si e ate mesnmo utlilizando outra tecnologia</q>
 </br>
 
 <!-- topicos da resoluçãao -->
---
+++ script.js
@@ -0,0 +1,50 @@
+let form = document.querySelector('form');
+
+// let nome
+           
+form.addEventListener('submit', function(e){
+
+  e.preventDefault();
+
+  
+  let url = "https://pokeapi.co/api/v2/pokemon/";
+
+  let nome = document.querySelector('#pokeinput');
+
+  let valorName = this.pokeinput.value.toLocaleLowerCase();
+
+  url = url + valorName
+
+  let content = document.querySelector('#pokeinformation')
+  let contentimg = document.querySelector('#pokeimg')
+
+  let html = ''
+
+  fetch(url)
+    .then(content => content.json())
+    .then(function(data) {
+      
+      html = `<p>Nome: ${data.name} 
+              </br> 
+              Tipo: ${data.types[0].type.name}</p>
+      `
+      content.innerHTML = html
+
+      html = `<img title='${data.name} N${data.id}' 
+              src='${data.sprites.front_default}'>
+
+              <img title='${data.name} N${data.id}' 
+              src='${data.sprites.back_default}'>`
+
+      contentimg.innerHTML = html
+
+      nome.value = ''
+
+    })
+    .catch(function(err){
+      content.innerHTML  = `<p>Ops😅, Pokemon "<u>${valorName.toLocaleUpperCase()}</u>" nao encontrado </br> tente outra vez</p>`
+
+      contentimg.innerHTML = ''
+
+    })
+});

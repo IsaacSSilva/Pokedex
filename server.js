6510d5ac2058400adca65da59c8cedb74155df6d
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-07-30 14:47:52 -0300

    🎉

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/assets/favicon.ico'));
 
 app.get('/', (req, res) => {
 
